cc8031b6a27c2ee9405c364eca66f0860c2b4112
Nathan Lyonnet <22300321@me.uvsq.fr>
2025-01-27 12:10:58 +0100

    doc main

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,20 +1,23 @@
+/**
+ * La classe Main contient le point d'entrée du programme. Elle crée des instances 
+ * de routes, d'inscriptions et de voiliers, enregistre les voiliers et affiche la 
+ * liste des voiliers inscrits.
+ */
 public class Main {
-    public static void main(String[] args) {
-            RouteAlize routeAlize = new RouteAlize();
-            RouteOrthodromie routeOrthodromie = new RouteOrthodromie();
+        public static void main(String[] args) {
+                RouteAlize routeAlize = new RouteAlize();
+                RouteOrthodromie routeOrthodromie = new RouteOrthodromie();
 
-            IncriptionMonocoque inscriptionMonocoque = new IncriptionMonocoque();
-            IncriptionMulticoque inscriptionMulticoque = new IncriptionMulticoque();
+                IncriptionMonocoque inscriptionMonocoque = new IncriptionMonocoque();
+                IncriptionMulticoque inscriptionMulticoque = new IncriptionMulticoque();
 
-            Voilier v1 = inscriptionMonocoque.factoryMethod("zebi",routeAlize,"sky1",EnumClass.EnumMono);
-            Voilier v2 = inscriptionMulticoque.factoryMethod("vroum",routeOrthodromie,"sky2",EnumClass.EnumMulti);
+                Voilier v1 = inscriptionMonocoque.factoryMethod("zebi", routeAlize, "sky1", EnumClass.EnumMono);
+                Voilier v2 = inscriptionMulticoque.factoryMethod("vroum", routeOrthodromie, "sky2", EnumClass.EnumMulti);
 
-            inscriptionMonocoque.enregistrement(v1);
-            inscriptionMulticoque.enregistrement(v2);
+                inscriptionMonocoque.enregistrement(v1);
+                inscriptionMulticoque.enregistrement(v2);
 
-            System.out.println(InscriptionVoilier.listeInscription);
-
-
+                // Affiche la liste des voiliers inscrits
+                System.out.println(InscriptionVoilier.listeInscription);
         }
-
 }
