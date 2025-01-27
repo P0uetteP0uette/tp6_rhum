2c628ed80f3d52d61bf4c76391628cdf6e010a4b
Nathan Lyonnet <22300321@me.uvsq.fr>
2025-01-27 12:08:12 +0100

    doc ArmementVelo

--- src/main/java/ArmementVelo.java
+++ src/main/java/ArmementVelo.java
@@ -1,10 +1,26 @@
+/**
+ * La classe ArmementVelo représente un type spécifique d'armement supplémentaire
+ * pour un voilier. Elle hérite de la classe ArmementSupplementaire.
+ */
 public class ArmementVelo extends ArmementSupplementaire {
 
+    /**
+     * Constructeur de la classe ArmementVelo.
+     *
+     * @param voilier Le voilier auquel cet armement supplémentaire sera associé.
+     */
     public ArmementVelo(Voilier voilier) {
         super(voilier);
     }
 
-    public String toString(){
+    /**
+     * Retourne une représentation sous forme de chaîne de caractères de cet
+     * armement.
+     *
+     * @return "ArmementVelo" - le nom de cette classe d'armement supplémentaire.
+     */
+    @Override
+    public String toString() {
         return "ArmementVelo";
     }
 }
