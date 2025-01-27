157e6668c01f65532e4ea4405f0b32663424ce2c
Nathan Lyonnet <22300321@me.uvsq.fr>
2025-01-27 12:08:44 +0100

    doc InscriptionMonocoque

--- src/main/java/IncriptionMonocoque.java
+++ src/main/java/IncriptionMonocoque.java
@@ -1,16 +1,33 @@
-public class IncriptionMonocoque extends InscriptionVoilier{
+/**
+ * La classe IncriptionMonocoque représente une inscription spécifique pour les voiliers monocoques.
+ * Elle hérite de la classe InscriptionVoilier.
+ */
+public class IncriptionMonocoque extends InscriptionVoilier {
 
+    /**
+     * Méthode de fabrique pour créer un voilier en fonction des paramètres fournis.
+     *
+     * @param nom Le nom du voilier.
+     * @param suivreRoute La stratégie de suivi de route du voilier.
+     * @param nomSkipper Le nom du skipper.
+     * @param classe La classe du voilier (EnumMono ou autre).
+     * @return Un objet Voilier, soit un Monocoque soit un Multicoque en fonction de la classe.
+     */
     @Override
-    public Voilier factoryMethod(String nom, SuivreRoute suivreRoute, String nomSkipper, EnumClass classe){
-        if(classe == EnumClass.EnumMono){
-            return new Monocoque(nom,suivreRoute,nomSkipper,classe);
+    public Voilier factoryMethod(String nom, SuivreRoute suivreRoute, String nomSkipper, EnumClass classe) {
+        if (classe == EnumClass.EnumMono) {
+            return new Monocoque(nom, suivreRoute, nomSkipper, classe);
         } else {
-            return new Multicoque(nom,suivreRoute,nomSkipper,classe);
+            return new Multicoque(nom, suivreRoute, nomSkipper, classe);
         }
     }
 
-    public void enregistrement(Voilier voilier){
+    /**
+     * Enregistre un voilier dans la liste des inscriptions.
+     *
+     * @param voilier Le voilier à enregistrer.
+     */
+    public void enregistrement(Voilier voilier) {
         listeInscription.add(voilier);
     }
-
 }
