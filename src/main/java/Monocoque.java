5b75760caf30c44cb8b4c3330ca2cd8580f6b950
Nathan Lyonnet <22300321@me.uvsq.fr>
2025-01-27 12:11:28 +0100

    doc Monocoque

--- src/main/java/Monocoque.java
+++ src/main/java/Monocoque.java
@@ -1,12 +1,28 @@
+/**
+ * La classe Monocoque représente un type spécifique de voilier monocoque.
+ * Elle hérite de la classe Voilier.
+ */
 public class Monocoque extends Voilier {
 
-    public Monocoque(String nom, SuivreRoute suivreRoute,String nomSkipper,EnumClass classe) {
-        super(nom,suivreRoute,nomSkipper,classe);
-
+    /**
+     * Constructeur de la classe Monocoque.
+     *
+     * @param nom Le nom du voilier.
+     * @param suivreRoute La stratégie de suivi de route du voilier.
+     * @param nomSkipper Le nom du skipper.
+     * @param classe La classe du voilier.
+     */
+    public Monocoque(String nom, SuivreRoute suivreRoute, String nomSkipper, EnumClass classe) {
+        super(nom, suivreRoute, nomSkipper, classe);
     }
 
-
+    /**
+     * Retourne une représentation sous forme de chaîne de caractères de ce voilier monocoque.
+     *
+     * @return Une chaîne de caractères représentant ce voilier monocoque.
+     */
+    @Override
     public String toString() {
-        return nom+" est monocoque";
+        return nom + " est monocoque";
     }
 }
