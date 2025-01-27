d6d8f78fb21135feb831ca873c928d981c27d085
Nathan Lyonnet <22300321@me.uvsq.fr>
2025-01-27 12:10:31 +0100

    doc InscriptionVoilier

--- src/main/java/InscriptionVoilier.java
+++ src/main/java/InscriptionVoilier.java
@@ -1,17 +1,47 @@
 import java.util.ArrayList;
 import java.util.List;
 
+/**
+ * La classe abstraite InscriptionVoilier représente un processus général
+ * d'inscription pour les voiliers. Elle contient une méthode abstraite
+ * factoryMethod pour créer des objets Voilier et une méthode pour enregistrer
+ * les voiliers inscrits.
+ */
 public abstract class InscriptionVoilier {
+    /**
+     * Liste des voiliers inscrits.
+     */
     static List<Voilier> listeInscription = new ArrayList<>();
 
+    /**
+     * Méthode de fabrique abstraite pour créer des voiliers en fonction des
+     * paramètres fournis.
+     *
+     * @param nom Le nom du voilier.
+     * @param suivreRoute La stratégie de suivi de route du voilier.
+     * @param nomSkipper Le nom du skipper.
+     * @param classe La classe du voilier.
+     * @return Un objet Voilier.
+     */
     public abstract Voilier factoryMethod(String nom, SuivreRoute suivreRoute, String nomSkipper, EnumClass classe);
 
-    void enregistrement(Voilier voilier){
+    /**
+     * Enregistre un voilier dans la liste des inscriptions.
+     *
+     * @param voilier Le voilier à enregistrer.
+     */
+    void enregistrement(Voilier voilier) {
         listeInscription.add(voilier);
     }
 
-    public String toString(){
+    /**
+     * Retourne une représentation sous forme de chaîne de caractères de la liste des
+     * voiliers inscrits.
+     *
+     * @return Une chaîne de caractères représentant la liste des voiliers inscrits.
+     */
+    @Override
+    public String toString() {
         return listeInscription.toString();
     }
-
 }
