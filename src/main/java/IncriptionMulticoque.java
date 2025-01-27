524a96492484fe2fa6be9ba1c26636326865d5b4
Nathan Lyonnet <22300321@me.uvsq.fr>
2025-01-27 12:09:44 +0100

    InscriptionMulticoque

--- src/main/java/IncriptionMulticoque.java
+++ src/main/java/IncriptionMulticoque.java
@@ -1,5 +1,18 @@
-public class IncriptionMulticoque extends InscriptionVoilier{
+/**
+ * La classe IncriptionMulticoque représente une inscription spécifique pour les voiliers multicoques.
+ * Elle hérite de la classe InscriptionVoilier.
+ */
+public class IncriptionMulticoque extends InscriptionVoilier {
 
+    /**
+     * Méthode de fabrique pour créer un voilier en fonction des paramètres fournis.
+     *
+     * @param nom Le nom du voilier.
+     * @param suivreRoute La stratégie de suivi de route du voilier.
+     * @param nomSkipper Le nom du skipper.
+     * @param classe La classe du voilier (EnumMono ou autre).
+     * @return Un objet Voilier, soit un Monocoque soit un Multicoque en fonction de la classe.
+     */
     @Override
     public Voilier factoryMethod(String nom, SuivreRoute suivreRoute, String nomSkipper, EnumClass classe) {
         if (classe == EnumClass.EnumMono) {
@@ -9,7 +22,12 @@ public class IncriptionMulticoque extends InscriptionVoilier{
         }
     }
 
-    public void enregistrement(Voilier voilier){
+    /**
+     * Enregistre un voilier dans la liste des inscriptions.
+     *
+     * @param voilier Le voilier à enregistrer.
+     */
+    public void enregistrement(Voilier voilier) {
         listeInscription.add(voilier);
     }
 }
