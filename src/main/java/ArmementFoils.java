d559f88f15080a6b0a337b88ad850a6867380877
Nathan Lyonnet <22300321@me.uvsq.fr>
2025-01-27 12:06:33 +0100

    doc ArmementFoils

--- src/main/java/ArmementFoils.java
+++ src/main/java/ArmementFoils.java
@@ -1,10 +1,26 @@
-public class ArmementFoils extends ArmementSupplementaire{
+/**
+ * La classe ArmementFoils représente un type spécifique d'armement supplémentaire
+ * pour un voilier. Elle hérite de la classe ArmementSupplementaire.
+ */
+public class ArmementFoils extends ArmementSupplementaire {
 
+    /**
+     * Constructeur de la classe ArmementFoils.
+     *
+     * @param voilier Le voilier auquel cet armement supplémentaire sera associé.
+     */
     public ArmementFoils(Voilier voilier) {
         super(voilier);
     }
 
-    public String toString(){
+    /**
+     * Retourne une représentation sous forme de chaîne de caractères de cet
+     * armement.
+     *
+     * @return "ArmementFoils" - le nom de cette classe d'armement supplémentaire.
+     */
+    @Override
+    public String toString() {
         return "ArmementFoils";
     }
 }
